3db00cf623d706d02c2dedd844e6eee2d92a1e9f
Felix Bogomolov <felibog@bk.ru>
2025-05-27 18:16:45 +0000

    save

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from os import getcwd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    with open("index.html", "r") as file:
+        return file.read()
+    
+@app.route("/langs/")
+def langs():
+    with open("langs/index.html", "r") as file:
+        return file.read()
+    
+@app.route("/langs/python")
+def python():
+    with open("langs/python/index.html", "r") as file:
+        return file.read()
+    
+app.run(port=8080)
